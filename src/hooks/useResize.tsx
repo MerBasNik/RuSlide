2cf611894002b4b3321986af1997a0eb18cdab6e
MerBasNik <patrusevvadim3@gmail.com>
2026-01-13 23:03:02 +0300

    fix

--- src/hooks/useResize.tsx
+++ src/hooks/useResize.tsx
@@ -57,7 +57,7 @@ export const useResize = ({ slideRef, slideId }: UseResizeProps) => {
     );
 
     const handleResizeMove = useCallback(
-        (moveEvent: React.MouseEvent) => {
+        (moveEvent: MouseEvent) => {
             if (
                 !isResizing ||
                 !resizedElementRef.current ||
